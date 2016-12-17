b4686b9d6ec453142079187ad3f0e3345559ee60
Anderson Mendes <lge500meu@gmail.com>
2016-12-16 19:53:39 -0500

    tesla

--- tesla.mk
+++ tesla.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 $(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 ## Device identifier. This must come after all inclusions
